5fef7b0b48cc8fb9309773cc10a6b11785497238
scorsi <sylvain.corsini@gmail.com>
2019-03-05 14:06:03 +0100

    Fixed define guardian names

--- faillist/src/validation/header/id_plane.h
+++ faillist/src/validation/header/id_plane.h
@@ -1,8 +1,8 @@
-#ifndef FAILLIST_VALIDATION_ID_PLANES_H
-#define FAILLIST_VALIDATION_ID_PLANES_H
+#ifndef FAILLIST_VALIDATION_HEADER_ID_PLANE_H
+#define FAILLIST_VALIDATION_HEADER_ID_PLANE_H
 
 #include "data/validated_data_header.h"
 
 int faillist_valid_id_plane(faillist_validated_data_header_t *, unsigned char []);
 
-#endif //FAILLIST_VALIDATION_ID_PLANES_H
+#endif //FAILLIST_VALIDATION_HEADER_ID_PLANE_H
--- faillist/src/validation/header/type_plane.h
+++ faillist/src/validation/header/type_plane.h
@@ -1,8 +1,8 @@
-#ifndef FAILLIST_VALIDATION_TYPE_PLANE_H
-#define FAILLIST_VALIDATION_TYPE_PLANE_H
+#ifndef FAILLIST_VALIDATION_HEADER_TYPE_PLANE_H
+#define FAILLIST_VALIDATION_HEADER_TYPE_PLANE_H
 
 #include "data/validated_data_header.h"
 
 int faillist_valid_type_plane(faillist_validated_data_header_t *, unsigned int);
 
-#endif //FAILLIST_VALIDATION_TYPE_PLANE_H
+#endif //FAILLIST_VALIDATION_HEADER_TYPE_PLANE_H
