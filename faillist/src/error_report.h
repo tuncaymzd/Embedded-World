834014845718d0d07e9df8485dd68caf80fec3d8
scorsi <sylvain.corsini@gmail.com>
2019-03-11 11:08:11 +0100

    Added error_report function with a main example
    Signed-off-by: scorsi <sylvain.corsini@gmail.com>

---
+++ faillist/src/error_report.h
@@ -0,0 +1,6 @@
+#ifndef FAILLIST_ERROR_REPORT_H
+#define FAILLIST_ERROR_REPORT_H
+
+void error_report(char **error);
+
+#endif //FAILLIST_ERROR_REPORT_H
--- faillist/src/main.c
+++ faillist/src/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "validation/header.h"
+#include "error_report.h"
 
 int main() {
-    faillist_parsed_data_header_t parsed_header = {
-            "7T-BED", 0x140, 0
-    };
-    faillist_validated_data_header_t validated_header;
-
-    if (faillist_valid_header(&validated_header, &parsed_header) == EXIT_SUCCESS) {
-        printf("Header is valid\n");
-        printf("%s\n%s\n%s\n", validated_header.id_plane, validated_header.type_plane, validated_header.nationality);
-    } else {
-        printf("Header is invalid");
-    }
+    char **error;
+    error = malloc(100 * sizeof(char *));
+    memset(error, '\0', 100 * sizeof(char *));
+    error[0] = strdup("YA-AAA");
+    error[1] = strdup("0x00000140");
+    error[2] = strdup("0x00000001");
+    error[3] = strdup("0x00001000");
+    error[4] = strdup("0x5c862eec");
+    error[5] = strdup("0x00002004");
+    error[6] = strdup("0x00000003");
+    error[7] = strdup("0x0000000b");
+    error[8] = strdup("ABCDEFGHIXYZ");
+    error_report(error);
     return 0;
 }
